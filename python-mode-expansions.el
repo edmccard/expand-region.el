453fa76424078f074f402627b4f32d9a949805aa
Ed McCardell <edmccard@hotmail.com>
2012-02-28 00:43:20 -0500

    Add proper tqs handling to python.el expansions

--- python-mode-expansions.el
+++ python-mode-expansions.el
@@ -1,8 +1,8 @@
 ;;; python-mode-expansions.el --- Python-specific expansions for expand-region
 
-;; Copyright (C) 2012 Ivan Andrus
+;; Copyright (C) 2012 Ivan Andrus, Ed McCardell
 
-;; Author: Ivan Andrus
+;; Author: Ivan Andrus, Ed McCardell
 ;; Based on js-mode-expansions by: Magnar Sveen <magnars@gmail.com>
 ;; Keywords: marking region
 
@@ -28,15 +28,48 @@
 ;;
 ;;     https://github.com/magnars/expand-region.el
 
-;;; Bugs:
-
-;; Doesn't properly handle triple quoted strings -- need to patch or
-;; replace regular er/mark-inside-pairs since it marks inside the
-;; outermost pair of quotes.
-
 ;;; Code:
 
 (require 'expand-region-core)
+
+(defun er--python-string-start-pos ()
+  "Returns character address of start of string, nil if not inside. "
+  (let ((pt (point)))
+    (save-excursion
+      ;; python-beginning-of-string returns the current
+      ;; position if point is not in a string, so we have
+      ;; to check if point moved, and if not, we check if
+      ;; we weren't already on the first quote of a string
+      (python-beginning-of-string)
+      (cond ((/= pt (point)) (point))
+            ((looking-at "\"\\|'") (point))
+            (t nil)))))
+
+(defun er/mark-inside-python-quotes ()
+  "Mark the inside of the current string, not including the quotation marks."
+  (interactive)
+  (let ((beg (er--python-string-start-pos)))
+    (when beg
+      (goto-char beg)
+      ;; Skipping quote characters one-by-one will do the wrong thing
+      ;; if a triple-quoted string ends with an escaped quote, e.g.
+      ;; """The last word is \"quoted.\""""
+      (let ((q-skip (if (looking-at "\"\"\"\\|'''") 3 1)))
+        (forward-sexp)
+        (backward-char q-skip)
+        (set-mark (point))
+        (goto-char beg)
+        (forward-char q-skip)))))
+
+(defun er/mark-outside-python-quotes ()
+  "Mark the current string, including the quotation marks."
+  (interactive)
+  (let ((beg (er--python-string-start-pos)))
+    (when beg
+      (goto-char beg)
+      (set-mark (point))
+      (forward-sexp)
+      (exchange-point-and-mark))))
 
 (defun er/mark-python-statement ()
   "Marks one Python statement, eg. x = 3"
@@ -47,10 +80,15 @@
 
 (defun er/add-python-mode-expansions ()
   "Adds Python-specific expansions for buffers in python-mode"
-  (set (make-local-variable 'er/try-expand-list) (append
-                                                  er/try-expand-list
-                                                  '(er/mark-python-statement
-                                                    python-mark-block))))
+  (set (make-local-variable 'er/try-expand-list)
+       (remove 'er/mark-inside-quotes
+               (remove 'er/mark-outside-quotes
+                       (append
+                        er/try-expand-list
+                        '(er/mark-python-statement
+                          er/mark-inside-python-quotes
+                          er/mark-outside-python-quotes
+                          python-mark-block))))))
 
 (add-hook 'python-mode-hook 'er/add-python-mode-expansions)
 
